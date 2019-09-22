9e04c501eb6780ecb7228d160a1e84ee41c8d623
Maksim Akinshin <otosigen@gmail.com>
2019-09-22 22:06:47 +0300

    main simple medium hard pages html+css

---
+++ main.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('button');
+const simpleDif = document.getElementById('simple')
+
+btn.addEventListener('click', () => {
+    if (simpleDif.value === 'simple') {
+
+    }
+})
