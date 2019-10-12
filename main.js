22b7b09d0e1b9b7b35947bd6ffa2d87ff069316b
Maksim Akinshin <otosigen@gmail.com>
2019-10-12 16:47:56 +0300

    add bug card

--- main.js
+++ main.js
@@ -1,55 +1,79 @@
 getById = id => document.getElementById(id);
 
 const mainPage = getById('choose-level-page');
-const levelEasy = getById('easy-lavel-page');
-const levelMedium = getById('medium-lavel-page');
-const levelHard = getById('hard-lavel-page');
+const levelEasyPage = getById('easy-level-page');
+const levelMediumPage = getById('medium-level-page');
+const levelHardPage = getById('hard-level-page');
+const startGameBtn = getById('button');
+
+const easyDifficulty = getById('easy');
+const mediumDifficulty = getById('medium');
+const hardDifficulty = getById('hard');
+
+const frontCardEasyBug = document.querySelectorAll('.easy .front');
+const frontCardMediumBug = document.querySelectorAll('.medium .front');
+const frontCardHardBug = document.querySelectorAll('.hard .front');
+
+let bugNumber;
 
 
 
 function chooseLevel() {
 
-    const btn = getById('button');
-    const level = document.getElementsByClassName('choose-difficulty');
-
-    for (i = 0; i < level.length; i++) {
-
-        btn.addEventListener('click', function () {
-            if (level[0].checked) {
-                mainPage.style.display = 'none';
-                levelEasy.style.display = 'flex';
-            }
-            else if (level[1].checked) {
-                mainPage.style.display = 'none';
-                levelMedium.style.display = 'flex';
-            }
-            else if (level[2].checked) {
-                mainPage.style.display = 'none';
-                levelHard.style.display = 'flex';
-            }
-        })
-    }
+    startGameBtn.addEventListener('click', function () {
+        if (easyDifficulty.checked) {
+            mainPage.style.display = 'none';
+            levelEasyPage.style.display = 'flex';
+            bugNumber = Math.floor(Math.random() * 3);
+            frontCardEasyBug[bugNumber].classList.add('bug');
+        }
+        else if (mediumDifficulty.checked) {
+            mainPage.style.display = 'none';
+            levelMediumPage.style.display = 'flex';
+            bugNumber = Math.floor(Math.random() * 6);
+            frontCardMediumBug[bugNumber].classList.add('bug');
+        }
+        else if (hardDifficulty.checked) {
+            mainPage.style.display = 'none';
+            levelHardPage.style.display = 'flex';
+            bugNumber = Math.floor(Math.random() * 10);
+            frontCardHardBug[bugNumber].classList.add('bug');
+        }
+    })
 }
-
 chooseLevel()
-
 
 
 function turnCard() {
 
-    const card = document.querySelectorAll('.card-back');
+    let card = document.getElementsByClassName('wrap');
 
-    card.forEach(function (item) {
-        item.addEventListener('click', function () {
-            if (this.className === 'card-back') {
-                this.classList.remove('card-back')
-                this.classList.add('card-front')
+    for (i = 0; i < card.length; i++) {
+
+        card[i].addEventListener('click', function () {
+
+            if (this.className === 'wrap') {
+                this.classList.add('rotated')
             }
-            else if (this.className === 'card-front') {
+            else {
+                this.className = 'wrap';
+            }
+        })
+    }
+}
+turnCard()
+
+function reloadGame() {
+
+    let cardFrontOrBug = document.getElementsByClassName('card front');
+
+    for (i = 0; i < cardFrontOrBug.length; i++) {
+
+        cardFrontOrBug[i].addEventListener('click', function () {
+            if (this.className === 'card front' || this.className === 'card front bug') {
                 document.location.reload(true);
             }
-        });
-    })
-};
-
-turnCard();
+        })
+    }
+}
+reloadGame()
