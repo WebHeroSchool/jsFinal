db6a8aa2cf13e743c6490b01791aab4e2bfa8507
Maksim Akinshin <otosigen@gmail.com>
2019-10-16 15:26:34 +0300

    reload game function

--- main.js
+++ main.js
@@ -14,26 +14,24 @@ const frontCardEasyBug = document.querySelectorAll('.easy .front');
 const frontCardMediumBug = document.querySelectorAll('.medium .front');
 const frontCardHardBug = document.querySelectorAll('.hard .front');
 
+const card = document.getElementsByClassName('wrap');
+
 let bugNumber;
-
-
+let numberOfClick = 0;
 
 function chooseLevel() {
-
     startGameBtn.addEventListener('click', function () {
         if (easyDifficulty.checked) {
             mainPage.style.display = 'none';
             levelEasyPage.style.display = 'flex';
             bugNumber = Math.floor(Math.random() * 3);
             frontCardEasyBug[bugNumber].classList.add('bug');
-        }
-        else if (mediumDifficulty.checked) {
+        } else if (mediumDifficulty.checked) {
             mainPage.style.display = 'none';
             levelMediumPage.style.display = 'flex';
             bugNumber = Math.floor(Math.random() * 6);
             frontCardMediumBug[bugNumber].classList.add('bug');
-        }
-        else if (hardDifficulty.checked) {
+        } else if (hardDifficulty.checked) {
             mainPage.style.display = 'none';
             levelHardPage.style.display = 'flex';
             bugNumber = Math.floor(Math.random() * 10);
@@ -46,33 +44,52 @@ chooseLevel()
 
 function turnCard() {
 
-    let card = document.getElementsByClassName('wrap');
-
     for (i = 0; i < card.length; i++) {
-
         card[i].addEventListener('click', function () {
-
             if (this.className === 'wrap easy' || this.className === 'wrap medium' || this.className === 'wrap hard') {
                 this.classList.add('rotated')
+                numberOfClick++
+                console.log(numberOfClick)
+            } else if (numberOfClick !== 0) {
+                location.reload()
             }
         })
     }
 }
 turnCard()
 
+
+
+// function reloadGame() {
+
+//     let cardFrontOrBug = document.getElementsByClassName('card');
+
+//     for (i = 0; i < cardFrontOrBug.length; i++) {
+
+//         cardFrontOrBug[i].addEventListener('click', function () {
+//             if (this.className === 'card front' || this.className === 'card front bug') {
+//                 // document.location.reload(true);
+//             }
+
+//         })
+//     }
+// }
+// reloadGame()
+
 function reloadGame() {
 
-    let cardFrontOrBug = document.getElementsByClassName('card');
+    const cardCount = document.querySelectorAll(`.card`);
 
-    for (i = 0; i < cardFrontOrBug.length; i++) {
+    cardCount.forEach((elem) => {
 
-        cardFrontOrBug[i].addEventListener('click', function () {
-            if (this.className === 'card front' || this.className === 'card front bug') {
-                document.location.reload(true);
-            }
+        elem.addEventListener('click', () => {
 
-        })
-    }
+            if (numberOfClick === 0) {
+                numberOfClick++;
+            } else {
+                // location.reload();
+            };
+        });
+    });
 }
 reloadGame()
-
